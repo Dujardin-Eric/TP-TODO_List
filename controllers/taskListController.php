6bd162f39c03158d7aedbbd0fb5d279eccef997c
Dujardin <Dujardin.Eric@outlook.fr>
2020-04-10 12:09:24 +0200

    requête et affichage de la liste des tâches

--- controllers/taskListController.php
+++ controllers/taskListController.php
@@ -1,5 +1,10 @@
 <?php
 
+//Requête sur la BD pour afficher la liste des tâches
+$recordSET = $pdo->query("SELECT * FROM view_tasks");
+$taskList = $recordSET->fetchAll();
+
+
 $pageTitle = "liste des tâches";
 $content = "taskList";
 
